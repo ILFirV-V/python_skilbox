badc955255f17c15561ad2011cb13f716a2fe250
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2022-10-05 13:04:59 +0500

    Добавлена еще одна сортировка в 10 задании 15 модуля

--- дз15/homework_10.py
+++ дз15/homework_10.py
@@ -8,10 +8,19 @@ def bubble_sort(array):
                 array[j], array[j+1] = array[j+1], array[j]
 
 
+def selection_sort(array):
+    len_array = len(array)
+    for i in range(len_array):
+        for j in range(i + 1, len_array):
+            if array[j] < array[i]:
+                array[j], array[i] = array[i], array[j]
+
+
+
 N = int(input("Введите количество чисел в массиве: "))
 array = []
 for i in range(N):
     array.append(randint(1, 99))
 print("Изначальный список:", array)
-bubble_sort(array)
+selection_sort(array)
 print("Отсортированный список:", array)
